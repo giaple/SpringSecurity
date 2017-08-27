f9eb4fc30db8884cb2a8a97c4b1cdf50bcf83d34
hhoang262 <hoanghahuy2602@gmail.com>
2017-08-27 22:48:38 +0700

    Initialize

---
+++ src/main/java/com/example/config/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.example.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	// password encoder reference implemented in WebMvcConfig.java
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	// data source implemented out of the box by Spring Boot.
+	// We only need to provide the database information in the
+	// application.properties file
+	@Autowired
+	private DataSource dataSource;
+
+	// Reference to user and role queries stored in application.properties file
+	@Value("${spring.queries.users-query}")
+	private String usersQuery;
+
+	@Value("${spring.queries.roles-query}")
+	private String rolesQuery;
+
+	// AuthenticationManagerBuilder provides a mechanism to get a user based on the
+	// password encoder
+	// , data source, user query and role query.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).authoritiesByUsernameQuery(rolesQuery)
+				.dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll()
+				.antMatchers("/registration").permitAll().antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
+				.authenticated().and().csrf().disable().formLogin().loginPage("/login").failureUrl("/login?error=true")
+				.defaultSuccessUrl("/admin/home").usernameParameter("email").passwordParameter("password").and()
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and()
+				.exceptionHandling().accessDeniedPage("/access-denied");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	}
+
+}
